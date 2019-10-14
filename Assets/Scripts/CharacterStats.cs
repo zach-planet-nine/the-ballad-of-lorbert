8e030490f878257b0241224ef16c925ddfe55088
Zach Babb <zach@planetnine.app>
2019-10-14 13:31:45 -0700

    Added save and load functionality

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public enum Characters
+{
+    Lorbert = 0,
+    Artro = 1,
+    IO = 2
+}
+
+public enum EquipLocations
+{
+    Weapon = 0,
+    Head = 1,
+    Body = 2,
+    Shield = 3,
+    Arms = 4,
+    Neck = 5,
+    Hands = 6,
+    Feet = 7
+}
+
+public class CharacterStats : MonoBehaviour
+{
+
+    public static CharacterStats characterStats;
+    private PartyData partyData = new PartyData();
+
+    private void Awake()
+    {
+        if (characterStats == null)
+        {
+            characterStats = this;
+            DontDestroyOnLoad(gameObject);
+        } else if(this != characterStats)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Save(string fileName)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/" + fileName);
+
+        bf.Serialize(file, partyData);
+        file.Close();
+        Debug.Log("File saved");
+    }
+
+    public void Load(string fileName)
+    {
+        if(File.Exists(Application.persistentDataPath + "/" + fileName))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/" + fileName, FileMode.Open);
+            partyData = (PartyData)bf.Deserialize(file);
+            file.Close();
+            Debug.Log("File loaded");
+        } else
+        {
+            Debug.Log("File could not be found.");
+        }
+    }
+
+    private void EquipOnLocation(CharacterData character, EquipLocations location, string ninea)
+    {
+        switch(location)
+        {
+            case EquipLocations.Weapon:
+                character.weapon = ninea;
+                break;
+            case EquipLocations.Head:
+                character.head = ninea;
+                break;
+            case EquipLocations.Body:
+                character.body = ninea;
+                break;
+            case EquipLocations.Shield:
+                character.shield = ninea;
+                break;
+            case EquipLocations.Arms:
+                character.arms = ninea;
+                break;
+            case EquipLocations.Neck:
+                character.neck = ninea;
+                break;
+            case EquipLocations.Hands:
+                character.hands = ninea;
+                break;
+            case EquipLocations.Feet:
+                character.feet = ninea;
+                break;
+        }
+    }
+
+    public void EquipOnCharacterOnLocation(Characters character, EquipLocations location, string ninea)
+    {
+        switch(character)
+        {
+            case Characters.Lorbert: EquipOnLocation(partyData.LorbertData, location, ninea);
+                break;
+            case Characters.Artro: EquipOnLocation(partyData.ArtroData, location, ninea);
+                break;
+            case Characters.IO: EquipOnLocation(partyData.IOData, location, ninea);
+                break;
+        }
+    }
+
+    public void LogPartyData()
+    {
+        Debug.Log(partyData);
+        Debug.Log(partyData.ArtroData);
+        Debug.Log(partyData.ArtroData.arms);
+    }
+}
+
+[Serializable]
+class PartyData
+{
+    public CharacterData LorbertData = new CharacterData("Lorbert");
+    public CharacterData ArtroData = new CharacterData("Artro");
+    public CharacterData IOData = new CharacterData("I-O");
+}
+
+[Serializable]
+class CharacterData
+{
+    public int health = 1000;
+    public int stamina = 300;
+    public int mp = 200;
+    public int strength = 50;
+    public int vitality = 50;
+    public int agility = 50;
+    public int dexterity = 50;
+    public int wisdom = 50;
+    public int aura = 50;
+    public int perception = 50;
+    public int luck = 50;
+    public string weapon = "";
+    public string head = "";
+    public string body = "";
+    public string neck = "";
+    public string hands = "";
+    public string arms = "";
+    public string shield = "";
+    public string feet = "";
+
+    private string name = "";
+
+    public CharacterData(string name)
+    {
+        this.name = name;
+        switch(name)
+        {
+            case "Lorbert": health = 1200;
+                stamina = 350;
+                mp = 120;
+                strength = 64;
+                vitality = 55;
+                agility = 45;
+                dexterity = 40;
+                wisdom = 40;
+                aura = 35;
+                perception = 50;
+                luck = 50;
+                break;
+            case "Artro": health = 1300;
+                stamina = 280;
+                mp = 240;
+                strength = 45;
+                vitality = 43;
+                agility = 55;
+                dexterity = 50;
+                wisdom = 65;
+                aura = 55;
+                perception = 50;
+                luck = 50;
+                break;
+            case "I-O":
+                health = 1100;
+                stamina = 320;
+                mp = 210;
+                strength = 53;
+                vitality = 45;
+                agility = 60;
+                dexterity = 55;
+                wisdom = 55;
+                aura = 60;
+                perception = 50;
+                luck = 50;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/StartMenuTester.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenuTester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(50, 100, 150, 60), "Start New"))
+        {
+
+        }
+        if(GUI.Button(new Rect(50, 160, 150, 60), "Continue"))
+        {
+
+        }
+        if(GUI.Button(new Rect(50, 220, 150, 60), "Load"))
+        {
+            CharacterStats.characterStats.LogPartyData();
+            CharacterStats.characterStats.Load("testfile.dat");
+            CharacterStats.characterStats.LogPartyData();
+        }
+        if(GUI.Button(new Rect(50, 280, 150, 60), "Settings"))
+        {
+            Debug.Log("Settings");
+        }
+        if(GUI.Button(new Rect(500, 110, 150, 60), "Test Save"))
+        {
+            CharacterStats.characterStats.EquipOnCharacterOnLocation(Characters.Artro, EquipLocations.Arms, "010100111101010");
+            CharacterStats.characterStats.Save("testfile.dat");
+        }
+    }
+}
