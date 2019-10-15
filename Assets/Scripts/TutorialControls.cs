9909be067bda9dfb650eea901773f5d4f8d5e16b
Zach Babb <zach@planetnine.app>
2019-10-15 13:46:02 -0700

    updated controls

--- Assets/Scripts/TutorialControls.cs
+++ Assets/Scripts/TutorialControls.cs
@@ -16,7 +16,17 @@ public class TutorialControls : MonoBehaviour
     public TextAsset boxAsset;
 
     private GameObject AlienWithPriority;
-    private int stateIndex;
+    private int tutorialIndex;
+
+    private List<string> tutorial = new List<string>
+    {
+        "Welcome to battle in The Ballad of Lorbert!",
+        "To start go ahead and tap on Lorbert.",
+        "When you tap on a character they will become the active character.",
+        "Once a character has become active you can tap on an enemy to attack it. Go ahead and try that now.",
+        "Good! When you attack, the attack will use some of your character's stamina. The lower the stamina, the weaker the attack.",
+        "Stamina and mp both recharge over time."
+    };
 
     // Start is called before the first frame update
     void Start()
@@ -24,13 +34,38 @@ public class TutorialControls : MonoBehaviour
         
     }
 
+    void RenderDialog(string dialog)
+    {
+        GUIStyle dialogStyle = new GUIStyle();
+        dialogStyle.font = (Font)Resources.Load("Orbitron-Bold");
+        dialogStyle.fontSize = 32;
+        Texture2D blueTexture = new Texture2D(1, 1);
+        blueTexture.SetPixel(0, 0, Color.blue);
+        blueTexture.Apply();
+        dialogStyle.normal.background = blueTexture;
+        dialogStyle.normal.textColor = Color.white;
+        dialogStyle.wordWrap = true;
+
+        GUI.Box(new Rect(150, 10, Screen.width - 300, 150), GUIContent.none, dialogStyle);
+        GUI.Label(new Rect(160, 20, Screen.width - 320, 130), dialog, dialogStyle);
+    }
+
     private void OnGUI()
     {
-        GUIStyle boxStyle = new GUIStyle();
-        Texture2D boxTexture = new Texture2D(100, 200);
-        boxTexture.LoadImage(boxAsset.bytes);
-        boxStyle.normal.background = boxTexture;
-        GUI.Box(new Rect(50, 40, 180, 300), "Here is a box", boxStyle);
+        string dialog = tutorial[tutorialIndex];
+        if(dialog.Length > 0)
+        {
+            RenderDialog(dialog);
+        }
+        if(tutorialIndex == 1)
+        {
+            GUIStyle boxStyle = new GUIStyle();
+            Texture2D boxTexture = new Texture2D(100, 200);
+            boxTexture.LoadImage(boxAsset.bytes);
+            boxStyle.normal.background = boxTexture;
+            GUI.Box(new Rect(50, 40, 180, 300), "Here is a box", boxStyle);
+        }
+        
     }
 
     // Update is called once per frame
@@ -39,9 +74,33 @@ public class TutorialControls : MonoBehaviour
         if(Input.GetMouseButtonUp(0))
         {
             RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-            if(hitInfo)
+            if (tutorialIndex == 1)
             {
-                switch(hitInfo.transform.gameObject.name)
+                Debug.Log(hitInfo);
+                Debug.Log(hitInfo.transform.gameObject.name);
+                if(hitInfo && hitInfo.transform.gameObject.name == "LorbertRest")
+                {
+                    LorbertRest.SetActive(false);
+                    LorbertActive.SetActive(true);
+                    ArtroRest.SetActive(true);
+                    ArtroActive.SetActive(false);
+                    IORest.SetActive(true);
+                    IOActive.SetActive(false);
+                    AlienWithPriority = LorbertActive;
+                    tutorialIndex += 1;
+                }
+            }
+            else if (tutorialIndex == 3)
+            {
+
+            }
+            else if (tutorialIndex < 7)
+            {
+                tutorialIndex += 1;
+            }
+            else if (hitInfo)
+            {
+                switch (hitInfo.transform.gameObject.name)
                 {
                     case "LorbertRest":
                         LorbertRest.SetActive(false);
